6591399e62392217944a49514008502a9830fe36
superprogrammersys <ahmedalaramadan1406@gmail.com>
2024-11-30 22:16:25 -0800

    Complete frontend improvements and fix bugs. The site is now functional.

--- website/settings.py
+++ website/settings.py
@@ -38,21 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "rest_framework",
-    "rest_framework_simplejwt",
     "main",
 ]
-REST_FRAMEWORK={
-"DEFAULT_AUTHENTICATION_CLASSES":(
-    "rest_framework_simplejwt.authentication.JWTAuthentication",
-),
-}
-from datetime import timedelta
-SIMPLE_JWT={
-    "ACCESS_TOKEN_LIFETIME":timedelta(minutes=60),
-    "REFRESH_TOKEN_LIFETIME":timedelta(days=1),
-
-}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
